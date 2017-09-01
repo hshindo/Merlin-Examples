6c16311ac3a7c7d8f91f6605ebe9481c417fc258
Hiroyuki Shindo <hshindo@outlook.com>
2017-09-01 21:25:46 +0900

    Fix ner model2

--- ner2/ner.jl
+++ ner2/ner.jl
@@ -9,7 +9,7 @@ function NER()
     words = h5read(wordembeds_file, "key")
     worddict = Dict(words[i] => i for i=1:length(words))
     chardict = Dict{String,Int}()
-    NER(worddict, chardict, BIO2(), nothing)
+    NER(worddict, chardict, BIO(), nothing)
 end
 
 function encode(ner::NER, words::Vector{String})
@@ -54,8 +54,6 @@ end
 function train(ner::NER, trainfile::String, testfile::String)
     train_w, train_c, train_t = readdata!(ner, trainfile)
     test_w, test_c, test_t = readdata!(ner, testfile)
-    #train_w, train_c, train_t = train_w[5:5], train_c[5:5], train_t[5:5]
-    #test_w, test_c, test_t = train_w, train_c, train_t
     info("# Training sentences:\t$(length(train_w))")
     info("# Testing sentences:\t$(length(test_w))")
     info("# Words:\t$(length(ner.worddict))")
@@ -64,49 +62,33 @@ function train(ner::NER, trainfile::String, testfile::String)
 
     wordembeds = h5read(wordembeds_file, "value")
     charembeds = randn(Float32, 20, length(ner.chardict)) * 0.01f0
-    ner.model = Model(wordembeds, charembeds, length(ner.tagset))
+    tagembeds = randn(Float32, 50, length(ner.tagset)) * 0.01f0
+    ner.model = Model(wordembeds, charembeds, tagembeds)
     opt = SGD()
-    epoch = 1
-    ispretrain = true
-    f = train_f_pre
-    while epoch <= 50
+    for epoch = 1:50
         println("Epoch:\t$epoch")
         opt.rate = 0.001 / (1 + 0.05*(epoch-1))
-        #opt.rate = 0.00075
-
-        if ispretrain && epoch == 11
-            println("Refinement...")
-            c1, c2 = 0, 0
-            for i = 1:length(train_w)
-                y = ner.model(train_w[i], train_c[i], false)
-                t = train_t[i]
-                for j = 1:size(y.data,2)
-                    tt = t.data[j]
-                    if y.data[tt,j] >= y.data[tt+1,j]
-                        c1 += 1
-                    else
-                        t.data[j] += 1
-                        c2 += 1
-                    end
-                end
-            end
-            epoch = 1
-            wordembeds = h5read(wordembeds_file, "value")
-            charembeds = randn(Float32, 20, length(ner.chardict)) * 0.01f0
-            ner.model = Model(wordembeds, charembeds, length(ner.tagset))
-            ispretrain = false
-            f = train_f
-            continue
-        end
+        # opt.rate = 0.00075
 
         train_data = makebatch(16, train_w, train_c, train_t)
-        loss = minimize!(x -> f(ner,x), opt, collect(zip(train_data...)))
+        function train_f(data::Tuple)
+            w, c, t = data
+            y = ner.model(w, c, t)
+            #softmax_crossentropy(t, y)
+        end
+        #train_data = collect(zip(train_w, train_c, train_t))
+        loss = minimize!(train_f, opt, collect(zip(train_data...)))
         println("Loss:\t$loss")
 
         # test
         println("Testing...")
+        function test_f(data::Tuple)
+            w, c = data
+            y = ner.model(w, c)
+            vec(argmax(y.data,1))
+        end
         test_data = collect(zip(test_w, test_c))
-        pred = cat(1, map(x -> test_f(ner,x), test_data)...)
+        pred = cat(1, map(test_f, test_data)...)
         gold = cat(1, map(t -> t.data, test_t)...)
         length(pred) == length(gold) || throw("Length mismatch.")
 
@@ -114,31 +96,7 @@ function train(ner::NER, trainfile::String, testfile::String)
         ranges_g = decode(ner.tagset, gold)
         fscore(ranges_g, ranges_p)
         println()
-        epoch += 1
     end
-end
-
-function train_f_pre(ner, data::Tuple)
-    w, c, t = data
-    y = ner.model(w, c, true)
-    tt = zeros(Float32, size(y.data))
-    for i = 1:length(t.data)
-        tt[t.data[i],i] = 0.5
-        tt[t.data[i]+1,i] = 0.5
-    end
-    softmax_crossentropy(Var(tt,t.batchdims), y)
-end
-
-function train_f(ner, data::Tuple)
-    w, c, t = data
-    y = ner.model(w, c, true)
-    softmax_crossentropy(t, y)
-end
-
-function test_f(ner, data::Tuple)
-    w, c = data
-    y = ner.model(w, c, false)
-    vec(argmax(y.data,1))
 end
 
 function fscore(golds::Vector{T}, preds::Vector{T}) where T
